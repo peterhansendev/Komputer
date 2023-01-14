822341b758fa607ffeef8d362225e51a2d54c86f
Peter <peterhansendev.com>
2023-01-14 16:27:28 +0100

    slug

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import { json } from "node:stream/consumers";
 
 const app = express();
 
-app.get('/api/item/:slug', (req, res) => {
-  res.send(" params Vercel api!" + req.params + JSON.stringify(req.params) + slug);
+app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
-console.log(JSON.stringify(req.params));
+  res.send(
+    " params Vercel api!" + req.params + JSON.stringify(req.params) + slug
+  );
   
 });
 
@@ -14,9 +15,8 @@ app.get("/api", (req, res) => {
   res.send("Express on Vercel api!");
 });
 
-
 app.get("/api/item/computerinfo", (req, res) => {
- res.send("computerinfo on Vercel");
+  res.send("computerinfo on Vercel");
   /* const { dynamic } = req.params
   const { key } = req.query
   console.log(dynamic, key); 
@@ -34,8 +34,6 @@ app.get("/api/item/computerinfo", (req, res) => {
 app.listen(3304, () => {
   console.log("Running on port 5000.");
 });
-
-
 
 // Export the Express API
 export default app;
