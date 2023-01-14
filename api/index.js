6e946880baf711cbd9565983c11ba0eb9263db80
Peter <peterhansendev.com>
2023-01-14 15:34:05 +0100

    api

--- api/index.js
+++ api/index.js
@@ -2,21 +2,33 @@ import express from "express";
 
 const app = express();
 
-const { v4 } = require('uuid');
-
-app.get('/api', (req, res) => {
-    res.send("Express on Vercel api");
+app.get("/api", (req, res) => {
+  res.send("Express on Vercel api!");
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get("/apii", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-app.get('/api1', (req, res) => {
-  res.send("Express on Vercel api");
+app.get("/api/computerinfo", (req, res) => {
+ res.send("computerinfo on Vercel");
+  /* const { dynamic } = req.params
+  const { key } = req.query
+  console.log(dynamic, key); 
+ 
+  res.status(200).send('<h1>ok</h1>')
+   client.query(`Select * from computers`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+      
+    }
+  });
+  client.end; */
 });
 
+app.listen(3304, () => {
+  console.log("Running on port 5000.");
+});
 
 
 
