e968a77ff5b8f3d051af49a4770a9b611863041e
Peter <peterhansendev.com>
2023-01-14 10:26:25 +0100

    vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,29 @@
+//PostgreSQL
+
+import pkg from "pg";
+const { Client } = pkg;
 import express from "express";
 const app = express();
+import dotenv from 'dotenv'
+dotenv.config()
 import { v4 } from 'uuid';
 
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  database: "",
+  password: process.env.POSTGRES_KEY,
+});
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(process.env.PORT || 3304 || 'https://komputer.vercel.app' || "komputer.vercel.app", '0.0.0.0', () => {
+  console.log("Sever is now listening at port 3300");
+});
+
+client.connect();
 
 app.get('/apii', (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -10,9 +32,35 @@ app.get('/apii', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get("/computerinfo", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).json({info: "1234"})
+  /* const { dynamic } = req.params
+  const { key } = req.query
+  console.log(dynamic, key); 
+ 
+  res.status(200).send('<h1>ok</h1>')
+   client.query(`Select * from computers`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+      
+    }
+  });
+  client.end; */
+});
+/*
+client.query(`Select * from computers`, (err, result) => {
+  if (!err) {
+    console.log(JSON.stringify(result.rows));
+  }
+}); */
+
+app.post("/", (req, res) => {
+  const { parcel } = req.body;
+  console.log(parcel.value);
+  if (!parcel) {
+    return res.status(400).send({ status: "failed" });
+  }
+  res.status(200).send({ status: "received" });
 });
 
-export default app;
