43aab847eec8e39e1433e66ba4a33b8adaf9b008
Peter <peterhansendev.com>
2023-01-14 11:28:04 +0100

    endpoint

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 
@@ -8,6 +10,34 @@ app.get("/api", (req, res) => {
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
+});
+
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  database: "",
+  password: process.env.POSTGRES_KEY,
+});
+
+client.connect();
+
+
+app.get("/computerinfo", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).json({info: "1234"})
+  /* const { dynamic } = req.params
+  const { key } = req.query
+  console.log(dynamic, key); 
+ 
+  res.status(200).send('<h1>ok</h1>')
+   client.query(`Select * from computers`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+      
+    }
+  });
+  client.end; */
 });
 
 // Export the Express API
