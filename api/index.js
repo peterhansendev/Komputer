7987112196195d4d8f0e034c755e1c644f2034d6
Peter <peterhansendev.com>
2023-01-14 15:33:03 +0100

    api

--- api/index.js
+++ api/index.js
@@ -5,10 +5,7 @@ const app = express();
 const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    res.send("Express on Vercel api");
 });
 
 app.get('/api/item/:slug', (req, res) => {
