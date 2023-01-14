3e13fcdfa936d55d069118b0681094f8ea7da7e8
Peter <peterhansendev.com>
2023-01-14 10:47:26 +0100

    vercel.json

--- connection.js
+++ connection.js
@@ -3,11 +3,7 @@ const app = express();
 import { v4 } from 'uuid';
 
 app.get('/api', (req, res) => {
-  res.status(200).json({info: "1234"})
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.send("Express on Vercel");
 });
 
 app.get('/api/item/:slug', (req, res) => {
@@ -15,4 +11,4 @@ app.get('/api/item/:slug', (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-export default app;
+module.exports = app;
