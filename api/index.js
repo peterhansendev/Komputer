9bd2f2e87162063f66a100df59f3a770891de3b8
Peter <peterhansendev.com>
2023-01-16 10:20:54 +0100

    pgadmin

--- api/index.js
+++ api/index.js
@@ -1,46 +1,23 @@
+import pgdata from "../pgadmin/pgadmin.js";
 import express from "express";
-const app = express();
+const app = express(); 
 
-app.listen( 3305, () => {
-  console.log("Sever is now listening at port 3300");
+app.listen(3305, () => {
+  console.log("Sever is now listening at port 3305");
 });
 
 app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params
+  const { slug } = req.params;
 
-  if(slug == "computerinfo") {
-    res.send(JSON.parse('{"name":"John", "age":30, "city":"New York"}'));
+  if (slug == "computerinfo") {
+    res.send(pgdata);
   } else {
-    res.send(
-      " params Vercel api!" + slug
-    );
+    res.send(" params Vercel api!" + slug);
   }
- 
-  
 });
 
 app.get("/api", (req, res) => {
   res.send("Express on Vercel api!");
 });
 
-/*
-app.get("/api/item/computerinfo", (req, res) => {
-  res.send("computerinfo on Vercel");
-   const { dynamic } = req.params
-  const { key } = req.query
-  console.log(dynamic, key); 
- 
-  res.status(200).send('<h1>ok</h1>')
-   client.query(`Select * from computers`, (err, result) => {
-    if (!err) {
-      res.send(result.rows);
-      
-    }
-  });
-  client.end; 
-});
-*/
-
-
-// Export the Express API
 export default app;
