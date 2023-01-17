5925e9366d3c2b0ae5d65ec1b94fa99d970f42f7
Peter <peterhansendev.com>
2023-01-17 08:43:29 +0100

    []

--- api/elephantsql.js
+++ api/elephantsql.js
@@ -6,7 +6,7 @@ dotenv.config({ path: "../.env" });
 
 const elephantsqlData = []
 
-var conString = `postgres://lxozqbnl:${process.env.ELEPHANT_KEY}@salt.db.elephantsql.com/lxozqbnl` //Can be found in the Details page
+var conString = `postgres://lxozqbnl:${process.env.ELEPHANT_KEY}@salt.db.elephantsql.com/lxozqbnl` 
 var client = new Client(conString);
 client.connect(function(err) {
   if(err) {
@@ -16,9 +16,8 @@ client.connect(function(err) {
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows);
+
     elephantsqlData.push(result.rows)
-    // >> output: 2018-08-23T14:02:57.117Z
     client.end();
   });
 });
